e70e1083894eaa2612890c5bbed01166ed62b8d2
svet <svetlana.prozhirova@megafon.ru>
2022-04-30 16:47:42 +0300

    add test api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
+import stripe
 from fastapi import FastAPI
+from .api.v1 import test
 
-app = FastAPI()
 
+app: FastAPI = FastAPI(
+    title='Api',
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+)
+
+app.include_router(
+    test.test,
+    prefix="/api/v1",
+    tags=[
+        "Kafka",
+    ],
+)
 
 @app.get('/')
 def index():
