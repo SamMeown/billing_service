a4325cec62c0a72889d2fcfed6341fb90c94a61e
svet <svetlana.prozhirova@megafon.ru>
2022-05-12 23:02:39 +0300

    update: subscriptions

--- app/api/v1/server/subscriptions.py
+++ app/api/v1/server/subscriptions.py
@@ -1,37 +1,100 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends
-from app.db.database import get_db
-from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+from fastapi.param_functions import Query
 from fastapi_pagination import Params
 from fastapi_pagination.ext.sqlalchemy import paginate
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
 from app.db.db_models import ModelSubscriptions
+from app.models.subscriptions import SubscriptionBase
 
 router = APIRouter()
-
-subscriptions = {
-    'subs_1m': {
-        'type': 'subscription',
-        'name': '1-Month Subscription',
-        'price': 12,
-    },
-    'subs_1y': {
-        'type': 'subscription',
-        'name': '1-Year Subscription',
-        'price': 120,
-    },
-}
+object = 'subscription'
 
 
 @router.get('/subscriptions')
-def fetch_subscriptions():
-    # Info about products
-    return [{**sub_info, 'id': sub_id} for sub_id, sub_info in subscriptions.items()]
+def fetch_subscriptions(
+        db: Session = Depends(get_db),
+        params: Params = Depends()
+) -> list:
+    _subscriptions = jsonable_encoder(paginate(db.query(ModelSubscriptions), params))
+    if _subscriptions.get('items') is not None:
+        return _subscriptions.get('items')
+    return []
 
-# @router.get('/subscriptions')
-# def fetch_subscriptions(
-#         db: Session = Depends(get_db),
-#         params: Params = Depends()
-# ) -> dict:
-#     return {
-#         "success": True,
-#         "response": paginate(db.query(ModelSubscriptions), params)
-#     }
+
+@router.get("/subscriptions/{id}")
+def get_id(
+        id: Optional[str] = Query(default=None, description=f"A unique {object} id"),
+        db: Session = Depends(get_db)
+) -> dict:
+    response = db.query(ModelSubscriptions).filter(ModelSubscriptions.id == id).first()
+    if response is None:
+        return {"response": f'The {object} not found'}
+    else:
+        return response
+
+
+@router.post("/subscriptions/create")
+def create(
+        name: Optional[str] = Query(default=None, description=f"A unique {object} name"),
+        description: Optional[str] = Query(default=None, description="Full description"),
+        price: Optional[int] = Query(default=0, description=f"Full {object} price"),
+        details: SubscriptionBase = {},
+        db: Session = Depends(get_db)
+) -> dict:
+    if db.query(ModelSubscriptions).filter(ModelSubscriptions.name == name).first() is not None:
+        return {
+            "success": False,
+            "response": f'The {object} already exists'
+        }
+    else:
+        to_create = ModelSubscriptions(
+            name=name,
+            description=description,
+            price=price
+        )
+        db.add(to_create)
+        db.commit()
+        return {
+            "success": True,
+            "response": to_create.id
+        }
+
+
+@router.put("/subscriptions/{id}/update")
+async def update(
+        id: Optional[str] = Query(default=None, description=f"A unique {object} id"),
+        name: Optional[str] = Query(default=None, description=f"A unique {object} name"),
+        description: Optional[str] = Query(default=None, description="Full description"),
+        price: Optional[int] = Query(default=0, description=f"Full {object} price"),
+        details: SubscriptionBase = {},
+        db: Session = Depends(get_db)
+) -> dict:
+    response = db.query(ModelSubscriptions).filter(ModelSubscriptions.id == id).first()
+    if response is None:
+        return {"response": f'The {object} not found'}
+    else:
+        response.name = name
+        response.description = description
+        response.price = price
+        db.add(response)
+        db.commit()
+        return {"response": id}
+
+
+@router.delete("/subscriptions/{id}/delete")
+def delete(
+        id: Optional[str] = Query(default=None, description=f"A unique {object} id"),
+        db: Session = Depends(get_db)
+) -> dict:
+    response = db.query(ModelSubscriptions).filter(ModelSubscriptions.id == id).first()
+    if response is None:
+        return {"response": f'The {object} not found'}
+    else:
+        db.query(ModelSubscriptions).filter(ModelSubscriptions.id == id).delete()
+        db.commit()
+        return {"response": id}
---
+++ app/models/subscriptions.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from app.models._base import BaseModel, Field
+
+
+class SubscriptionBase(BaseModel):
+    name: Optional[str] = Field(
+        default="",
+        description="Subscription name")
+    description: Optional[str] = Field(
+        default="",
+        description="Subscription description")
+    price: Optional[int] = Field(
+        default=0,
+        description="Subscription price")
+
+    class Config:
+        title = "Basic subscription model"
+        schema_extra = {
+            "example": {
+                **BaseModel.Config.schema_extra["example"],
+                "name": "1-Month Subscription",
+                "description": "Subscription for 4 people",
+                "price": 12
+            }
+        }
